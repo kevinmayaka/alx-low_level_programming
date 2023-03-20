df725d42852e18e79fd1c9da67ae6550ec2620d9
kevinmayaka <kevinabuga3@gmail.com>
2023-03-20 09:30:58 -0700

    How many legs does a dog have if you call his tail a leg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,36 +1,24 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdio.h>
+	#include <stdlib.h>
+	#include "dog.h"
 
 
 	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
+	 * print_dog - prints a struct dog
+	 * @d: struct dog to print
 	 */
-	struct dog
+	void print_dog(struct dog *d)
 	{
-		char *name;
-		float age;
-		char *owner;
-	};
+		if (d == NULL)
+			return;
 
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
-
-	#endif
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
 
